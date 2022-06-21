fa9437c6a259de6ed8cdcfa94095f6af501e8cdb
UBCh <u2b127@gmail.com>
2022-06-21 16:34:05 +0300

    jar

--- src/main/java/userInterfase/UserInterfase.java
+++ src/main/java/userInterfase/UserInterfase.java
@@ -1,17 +1,6 @@
 package userInterfase;
 
-import endgin.Analisator;
-import endgin.BruteForse;
-import endgin.Encoder;
-import endgin.Key;
 import graphicalInterface.Window;
-
-
-import java.util.Scanner;
-
-import static endgin.Encoder.*;
-import static java.lang.System.in;
-import static java.lang.System.out;
 
 public class UserInterfase {
 
